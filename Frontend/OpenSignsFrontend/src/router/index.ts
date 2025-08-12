ad8646697dd42751ea82466677df358d3e6f67ac
Lorenzo Fenderico <lorenzoFenderico.dev@gmail.com>
2025-08-12 14:36:36 +0200

    Hero section first layout

--- Frontend/OpenSignsFrontend/src/router/index.ts
+++ Frontend/OpenSignsFrontend/src/router/index.ts
@@ -1,8 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import HeroSection from '@/view/HeroSection.vue'
+import DashboardSection from '@/view/DashboardSection.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HeroSection,
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: DashboardSection
+    }
+  ],
 })
 
 export default router
---
+++ Frontend/OpenSignsFrontend/src/view/HeroSection.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="h-[100vh] bg-black/90 flex flex-col justify-center">
+    <h1 class="text-white mx-6 text-center pb-4 text-2xl">Unlock a <strong class="text-green-600 font-bold">new way to connect</strong>, <strong class="text-green-600 font-bold">express</strong>, and <strong class="text-green-600 font-bold">understand others</strong></h1>
+
+    <p class="text-white text-center mx-6 text-[10px]">Learn sign language with clarity and purpose. Build real connections, express yourself fully, and understand others in a more meaningful way.</p>
+
+    <router-link class="m-8 p-4 bg-green-700 text-white font-bold rounded-xl shadow-lg shadow-green-900 text-center transition active:scale-120 active:translate-y-4 hover:-translate-y-2" to="/dashboard">
+      <button>Start now!</button>
+    </router-link>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+
+</style>
